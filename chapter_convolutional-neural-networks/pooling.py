50d077c6fa945b168958d65b6698d3dd7138bb6f
zq <zhengqian0782@163.com>
2023-08-21 13:46:33 +0800

    update pooling.py

---
+++ chapter_convolutional-neural-networks/pooling.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+def pool2d(X, pool_size, mode='max'):
+    p_h, p_w = pool_size
+    Y = torch.zeros((X.shape[0] - p_h + 1, X.shape[1] - p_w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            if mode == 'max':
+                Y[i,j] = X[i:i + p_h, j:j + p_w].max()
+            elif mode == 'avg':
+                Y[i, j] = X[i:i + p_h, j:j + p_w].mean()
+    return Y
+
+
+X = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]])
+print(pool2d(X,(2, 2)))
+print(pool2d(X,(2,2),'avg'))
+
+
+X = torch.arange(16, dtype=torch.float32).reshape((1, 1, 4, 4))
+print(X)
+
+pool2d = nn.MaxPool2d(3)
+print(pool2d(X))
+
+pool2d = nn.MaxPool2d(3,padding=1,stride=2)
+print(pool2d(X))
+
+pool2d = nn.MaxPool2d((2,3),padding=(1,1),stride=(2,3))
+print(pool2d(X))
+
+X = torch.cat((X, X + 1),dim=1)
+print(X)
+
+pool2d = nn.MaxPool2d(3,padding=1,stride=2)
+print(pool2d(X))
